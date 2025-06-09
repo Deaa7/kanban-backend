c5cf5ff79ba908c6ea3c98496b92cf8674ff8d5b
Deaa <deaanaser209@gmail.com>
2025-06-10 02:25:12 +0300

    update2

--- kanban/settings.py
+++ kanban/settings.py
@@ -83,12 +83,14 @@ WSGI_APPLICATION = 'kanban.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kanban',
+        'USER': 'root',
+        'PASSWORD':'Deaa123321!',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
